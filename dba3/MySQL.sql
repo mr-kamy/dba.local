13be4e8835552628f8292962f979aacea0a083a7
kamc <steam-punker@mail.ru>
2018-05-17 10:38:10 +0300

    Update MySQL.sql

--- dba3/MySQL.sql
+++ dba3/MySQL.sql
@@ -8,4 +8,42 @@ CREATE TABLE `products` (
   `date`     DATE,
   `qt`       INT UNSIGNED,
   PRIMARY KEY (`id`)
-)
+);
+
+-- 10 самых новых товаров
+EXPLAIN SELECT * FROM `products`
+ORDER BY `date` DESC LIMIT 10;
+/*
+Запрос выполнялся 45ms
+Для нахождения результата используется сканирование всей таблицы
+ */
+
+
+-- 10 самых дешевых товаров
+EXPLAIN SELECT * FROM `products`
+ORDER BY `price` LIMIT 10;
+/*
+Запрос выполнялся 69ms
+Для нахождения результата используется сканирование всей таблицы
+ */
+
+--  10 товаров, цена на которых была максимально снижена
+EXPLAIN SELECT * FROM `products`
+ORDER BY `oldprice` - `price` DESC LIMIT 10;
+/*
+Запрос выполнялся 49ms
+Для нахождения результата используется сканирование всей таблицы
+ */
+
+-- Выбирают те товары, чей артикул начинается с символов "TEST"
+EXPLAIN SELECT * FROM `products`
+WHERE `art` LIKE 'TEST%';
+/*
+Запрос выполнялся 109ms
+Для нахождения результата используется сканирование всей таблицы
+Добавим индекс полю артикул
+Запрос выполнялся 58ms
+Сканировалась вся таблица
+Добавленный индекс показывается в поле possible keys.
+ */
+ALTER TABLE `products` ADD INDEX `art_index` (`art`);
